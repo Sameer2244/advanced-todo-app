b32308c690829e3ced94d9f552ea77d661ee3792
sameersk <sk5276873@gmail.com>
2025-10-05 12:33:13 +0530

    completed upcoming task section

--- src/components/UpcomingTasks.tsx
+++ src/components/UpcomingTasks.tsx
@@ -14,21 +14,26 @@ export default async function UpcomingTasks() {
       </h3>
       <div className="flex flex-col gap-4 py-4">
         {upcomingProjects?.projects?.length > 0 ? (
-          upcomingProjects?.projects?.map((p) => (
-            <Card key={p._id.toString()}>
-              <CardContent className="flex flex-col gap-2">
-                <div className="flex gap-2 items-center">
-                  <NotebookIcon />
-                  <div className="text-lg font-semibold">
-                    {p.title}
-                    <p className="text-muted-foreground text-sm">
-                      Due on : {new Date(p.dueDate!).toDateString()}
-                    </p>
+          upcomingProjects?.projects
+            ?.toSorted((p1, p2) =>
+              new Date(p1.dueDate!) > new Date(p2.dueDate!) ? 1 : -1
+            )
+            .slice(0, 3)
+            .map((p) => (
+              <Card key={p._id.toString()}>
+                <CardContent className="flex flex-col gap-2">
+                  <div className="flex gap-2 items-center">
+                    <NotebookIcon />
+                    <div className="text-lg font-semibold">
+                      {p.title}
+                      <p className="text-muted-foreground text-sm">
+                        Due on : {new Date(p.dueDate!).toDateString()}
+                      </p>
+                    </div>
                   </div>
-                </div>
-              </CardContent>
-            </Card>
-          ))
+                </CardContent>
+              </Card>
+            ))
         ) : (
           <p className="leading-7 [&:not(:first-child)]:mt-6">
             No projects available! please create your first projects from quick
